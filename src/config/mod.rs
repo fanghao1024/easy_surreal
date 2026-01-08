3388376075bba0d96b5689f9cb26f69ae326b7a1
hao fang <chinafanghao@foxmail.com>
2026-01-09 00:59:52 +0800

    Update mod.rs

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,9 @@
 
 use serde::{Serialize,Deserialize};
+use serde::de::DeserializeOwned;
 use serde_json::Value;
 use surrealdb::opt::auth::{Credentials,Jwt};
+use self::auth::AuthCredentials;
 pub mod parser;
 mod auth;
 
@@ -23,5 +25,75 @@ pub trait AuthBridger<'a,Action>{
     fn keys()->Vec<&'a str>;
 
     ///Convert to low-level instance and doesn't cumstom itself
-    fn to_lower(&'a self)->impl Credentials<Action,Jwt>
+    fn to_lower(&'a self)->impl Credentials<Action,Jwt>;
+}
+
+//SurrealDB的配置
+#[derive(Debug,Serialize,Clone)]
+pub struct SurrealConfig {
+    //Startup url
+    endpoint:String,
+    //Startup point
+    port:u16,
+    //Login credential data
+    auth:Value,
+}
+
+// convert serde_json::Value to SurrealConfig
+impl From<Value> for SurrealConfig {
+    fn from(value: Value) -> Self {
+        let endpoint=value.get("endpoint").unwrap().as_str().unwrap().to_string();
+        let port=value.get("port").unwrap().as_u64().unwrap() as u16;
+        let auth=value.get("auth").unwrap().clone();
+        Self{
+            endpoint,
+            port,
+            auth,
+        }
+    }
+}
+
+impl SurrealConfig{
+    ///Retrieves login credential data
+    /// All credentials can actually be converted
+    /// In fact, users may have no idea what type of login crediential they are
+    /// @return AuthCredientials
+    pub fn get_auth<P>(&self)->AuthCredientials<P>
+    where
+        P:Serialize+DeserializeOwned
+    {
+        let res:AuthCredentials<P>=self.auth.clone().into();
+        res
+    }
+
+    /// Get the configured SurrealDB address
+    pub fn get_endpoint(&self)->&str{
+        &self.endpoint
+    }
+
+    /// Get the configured SurrealDB port
+    pub fn get_port(&self)->u16{
+        self.port
+    }
+
+    /// Get URL, the actural format is {{address}}:{{port}}
+    pub fn url(&self)->String{
+        format!("{}:{}",self.endpoint,self.port)
+    }
+}
+
+#[cfg(test)]
+mod test_config{
+    use serde_json::Value;
+    use crate::config::auth::AuthCredentials;
+    use super::{parser::Parsers,SurrealConfig};
+
+    #[test]
+    fn test_parser_config(){
+        let json=Parsers::Json.parse(None);
+        let config:SurrealConfig=json.into();
+        dbg!(&config);
+        let auth_credential:AuthCredentials<Value>=config.get_auth();
+        dbg!(&auth_credential);
+    }
 }
